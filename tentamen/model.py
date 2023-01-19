9883f499351823b02f13f2a383f24b047cd5e712
Laura <laurakiwiet@gmail.com>
2023-01-19 11:57:13 +0000

    GRU model in model.py toegevoegd

--- tentamen/model.py
+++ tentamen/model.py
@@ -34,6 +34,25 @@ class Linear(nn.Module):
         return x
 
 
+class GRUmodel(nn.Module):
+    def __init__(self, config: Dict) -> None:
+        super().__init__()
+        self.rnn = nn.GRU(
+            input_size=config["input_size"],
+            hidden_size=config["hidden_size"],
+            dropout=config["dropout"],
+            batch_first=True,
+            num_layers=config["num_layers"],
+        )
+        self.linear = nn.Linear(config["hidden_size"], config["output_size"])
+
+    def forward(self, x: Tensor) -> Tensor:
+        x, _ = self.rnn(x)
+        last_step = x[:, -1, :]
+        yhat = self.linear(last_step)
+        return yhat
+
+
 class Accuracy:
     def __repr__(self) -> str:
         return "Accuracy"
